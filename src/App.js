78d02a75d0a8ebe7200921d6d4e9b63ca7a835ad
AlessandroVerdad <alleacco01@gmail.com>
2024-04-07 00:04:43 +0200

    working game!

--- src/App.js
+++ src/App.js
@@ -8,23 +8,60 @@ function Square({ value, onSquareClick }) {
   );
 }
 
+function calculateWinner(squares) {
+  const winLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < winLines.length; i++) {
+    const [a, b, c] = winLines[i];
+
+    if (squares[a] && squares[a] == squares[b] && squares[b] == squares[c])
+      return squares[a];
+  }
+
+  return null;
+}
+
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 export default function Board() {
-  //  Functions
+
+  //  Handlers
   function handleClick(i) {
+    //  Return early if square is already filled OR there's already a winner
+    if (squares[i] || calculateWinner(squares))
+      return;
+
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+
+    if (xIsNext) nextSquares[i] = "X";
+    else nextSquares[i] = "O";
+
+    setXIsNext(!xIsNext);
     setSquares(nextSquares);
   }
+
 
   //  States
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+
   //  Return
   return (
     <>
+
+      <div className="status">{getStatus(squares, xIsNext)}</div>
+
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -44,4 +81,14 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+
+function getStatus(squares, xIsNext) {
+  const winner = calculateWinner(squares);
+
+  if (winner)
+    return `Winner: ${winner}`;
+  else
+    return "Next player: " + (xIsNext ? "X" : "O");
 }
